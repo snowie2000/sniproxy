751f5196289c859f770b39d55c333d14d90d8e98
phus <phus.lu@gmail.com>
2013-04-22 03:37:22 +0800

    add dns server support

--- sniproxy.py
+++ sniproxy.py
@@ -24,6 +24,7 @@ import socket
 import errno
 import select
 import struct
+import random
 import logging
 
 
@@ -70,6 +71,142 @@ def forward_socket(local, remote, timeout=60, tick=2, bufsize=8192, maxping=None
             remote.close()
 
 
+class DNSUtil(object):
+    """
+    http://gfwrev.blogspot.com/2009/11/gfwdns.html
+    http://zh.wikipedia.org/wiki/域名服务器缓存污染
+    http://support.microsoft.com/kb/241352
+    """
+    blacklist = set([
+                    # for ipv6
+                    '1.1.1.1', '255.255.255.255',
+                    # for google+
+                    '74.125.127.102', '74.125.155.102', '74.125.39.102', '74.125.39.113',
+                    '209.85.229.138',
+                    # other ip list
+                    '128.121.126.139', '159.106.121.75', '169.132.13.103', '192.67.198.6',
+                    '202.106.1.2', '202.181.7.85', '203.161.230.171', '203.98.7.65',
+                    '207.12.88.98', '208.56.31.43', '209.145.54.50', '209.220.30.174',
+                    '209.36.73.33', '211.94.66.147', '213.169.251.35', '216.221.188.182',
+                    '216.234.179.13', '243.185.187.39', '37.61.54.158', '4.36.66.178',
+                    '46.82.174.68', '59.24.3.173', '64.33.88.161', '64.33.99.47',
+                    '64.66.163.251', '65.104.202.252', '65.160.219.113', '66.45.252.237',
+                    '72.14.205.104', '72.14.205.99', '78.16.49.15', '8.7.198.45', '93.46.8.89',
+                    ])
+    max_retry = 3
+    max_wait = 3
+
+    @staticmethod
+    def _reply_to_iplist(data):
+        assert isinstance(data, basestring)
+        iplist = ['.'.join(str(ord(x)) for x in s) for s in re.findall('\xc0.\x00\x01\x00\x01.{6}(.{4})', data) if all(ord(x) <= 255 for x in s)]
+        return iplist
+
+    @staticmethod
+    def is_bad_reply(data):
+        assert isinstance(data, basestring)
+        iplist = ['.'.join(str(ord(x)) for x in s) for s in re.findall('\xc0.\x00\x01\x00\x01.{6}(.{4})', data) if all(ord(x) <= 255 for x in s)]
+        iplist += ['.'.join(str(ord(x)) for x in s) for s in re.findall('\x00\x01\x00\x01.{6}(.{4})', data) if all(ord(x) <= 255 for x in s)]
+        return any(x in DNSUtil.blacklist for x in iplist)
+
+    @staticmethod
+    def _remote_resolve(dnsserver, qname, timeout=None):
+        if isinstance(dnsserver, tuple):
+            dnsserver, port = dnsserver
+        else:
+            port = 53
+        for i in xrange(DNSUtil.max_retry):
+            host = ''.join(chr(len(x))+x for x in qname.split('.'))
+            seqid = os.urandom(2)
+            data = '%s\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00%s\x00\x00\x01\x00\x01' % (seqid, host)
+            address_family = socket.AF_INET6 if ':' in dnsserver else socket.AF_INET
+            sock = None
+            try:
+                if i < DNSUtil.max_retry-1:
+                    # UDP mode query
+                    sock = socket.socket(family=address_family, type=socket.SOCK_DGRAM)
+                    sock.settimeout(timeout)
+                    sock.sendto(data, (dnsserver, port))
+                    for i in xrange(DNSUtil.max_wait):
+                        data = sock.recv(512)
+                        if data and not DNSUtil.is_bad_reply(data):
+                            return data[2:]
+                        else:
+                            logging.warning('DNSUtil._remote_resolve(dnsserver=%r, %r) return poisoned udp data=%r', qname, dnsserver, data)
+                else:
+                    # TCP mode query
+                    sock = socket.socket(family=address_family, type=socket.SOCK_STREAM)
+                    sock.settimeout(timeout)
+                    sock.connect((dnsserver, port))
+                    data = struct.pack('>h', len(data)) + data
+                    sock.send(data)
+                    rfile = sock.makefile('r', 512)
+                    data = rfile.read(2)
+                    if not data:
+                        logging.warning('DNSUtil._remote_resolve(dnsserver=%r, %r) return bad tcp header data=%r', qname, dnsserver, data)
+                    data = rfile.read(struct.unpack('>h', data)[0])
+                    if data and not DNSUtil.is_bad_reply(data):
+                        return data[2:]
+                    else:
+                        logging.warning('DNSUtil._remote_resolve(dnsserver=%r, %r) return bad tcp data=%r', qname, dnsserver, data)
+            except socket.error as e:
+                if e[0] in (10060, 'timed out'):
+                    continue
+            except Exception as e:
+                raise
+            finally:
+                if sock:
+                    sock.close()
+
+    @staticmethod
+    def remote_resolve(dnsserver, qname, timeout=None):
+        data = DNSUtil._remote_resolve(dnsserver, qname, timeout)
+        iplist = DNSUtil._reply_to_iplist(data or '')
+        return iplist
+
+
+class DNSServer(gevent.server.DatagramServer):
+    """DNS Proxy over TCP to avoid DNS poisoning"""
+    dnsservers = ['8.8.8.8', '8.8.4.4']
+    max_wait = 1
+    max_retry = 2
+    max_cache_size = 20000
+    timeout = 6
+
+    def __init__(self, *args, **kwargs):
+        gevent.server.DatagramServer.__init__(self, *args, **kwargs)
+        self.cache = {}
+
+    def add_record(self, qname, ip):
+        domain = ''.join(chr(len(x))+x for x in qname.split('.'))
+        record = '\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00'
+        record += domain
+        record += '\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00\x00\x00\n\x00'
+        record += '\x04' + ''.join(chr(int(x)) for x in ip.split('.'))
+        self.cache[domain] = record
+
+    def handle(self, data, address):
+        reqid = data[:2]
+        domain = data[12:data.find('\x00', 12)]
+        if len(self.cache) > self.max_cache_size:
+            self.cache.clear()
+        if domain not in self.cache:
+            qname = re.sub(r'[\x01-\x29]', '.', domain[1:])
+            try:
+                dnsserver = random.choice(self.dnsservers)
+                logging.info('DNSServer resolve domain=%r by dnsserver=%r to iplist', qname, dnsserver)
+                data = DNSUtil._remote_resolve(dnsserver, qname, self.timeout)
+                if not data:
+                    logging.warning('DNSServer resolve domain=%r return data=%s', qname, data)
+                    return
+                iplist = DNSUtil._reply_to_iplist(data)
+                self.cache[domain] = data
+                logging.info('DNSServer resolve domain=%r return iplist=%s', qname, iplist)
+            except socket.error as e:
+                logging.error('DNSServer resolve domain=%r to iplist failed:%s', qname, e)
+        return self.sendto(reqid + self.cache[domain], address)
+
+
 class SNIProxyHandler(object):
     """SNI Proxy Handler"""
 
@@ -105,7 +242,8 @@ class SNIProxyHandler(object):
         if server_name:
             logging.info('%s:%d forward to %r', address[0], address[1], server_name)
             # TODO: DNS cache
-            remote_iplist = socket.gethostbyname_ex(server_name)[-1]
+            # remote_iplist = socket.gethostbyname_ex(server_name)[-1]
+            remote_iplist = DNSUtil.remote_resolve('8.8.8.8', server_name, timeout=timeout)
             remote_ip = remote_iplist[0]
             remote_sock = socket.create_connection((remote_ip, 443), timeout=timeout)
             remote_sock.send(packet)
@@ -115,12 +253,30 @@ class SNIProxyHandler(object):
             logging.debug('%s:%d read_sni return server_name=%r', address[0], address[1], server_name)
 
 
+def get_listen_ip():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect(('8.8.8.8', 53))
+    listen_ip = sock.getsockname()[0]
+    sock.close()
+    return listen_ip
+
+
+def init_dnsmap(dns_server):
+    domains = ['twitter.com', 'api.twitter.com', 'platform.twitter.com', 'mobile.twitter.com']
+    listen_ip = get_listen_ip()
+    for domain in domains:
+        dns_server.add_record(domain, listen_ip)
+
+
 def main():
-    port = int(sys.argv[1]) if len(sys.argv) == 2 else 443
     logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(asctime)s %(message)s', datefmt='[%b %d %H:%M:%S]')
-    server = gevent.server.StreamServer(('', port), SNIProxyHandler)
-    logging.info('serving at %r', server.address)
-    server.serve_forever()
+    dns_server = DNSServer(('', 53))
+    init_dnsmap(dns_server)
+    logging.info('serving at %r', dns_server.address)
+    dns_server.start()
+    sni_server = gevent.server.StreamServer(('', 443), SNIProxyHandler)
+    logging.info('serving at %r', sni_server.address)
+    sni_server.serve_forever()
 
 if __name__ == '__main__':
     main()
